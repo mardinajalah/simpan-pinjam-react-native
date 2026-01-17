5b7ba845c9ff2d6a023e40b3163e26ee1c23b2ab
mardin <tredingb25@gmail.com>
2026-01-17 19:28:50 +0700

    feat: enhance CurrentLoanCart component with loan details and progress bar

--- components/CurrentLoanCart.tsx
+++ components/CurrentLoanCart.tsx
@@ -2,10 +2,39 @@ import React from 'react';
 import { Text, View } from 'react-native';
 
 const CurrentLoanCart = () => {
+  const totalLoan = 3000000;
+  const paidLoan = 1000000;
+  const remainingLoan = totalLoan - paidLoan;
+
+  const progress = Math.floor((paidLoan / totalLoan) * 100);
+
   return (
-    <View className='bg-white px-4 py-3 mx-5 mt-10 rounded-2xl elevation-sm'>
-      <Text>Pinjaman Saat Ini</Text>
-      <Text>Rp 3.000.000</Text>
+    <View className='bg-white mx-5 mt-10 rounded-2xl elevation-sm'>
+      <View className='flex-row justify-end'>
+        <View className='bg-red-600 py-2 w-1/3 items-center rounded-tr-2xl rounded-bl-2xl'>
+          <Text className='text-white font-bold'>Pinjaman</Text>
+        </View>
+      </View>
+      <View className='px-3 pb-6'>
+        <Text className='text-lg font-medium text-[#3e3e3e] text-center'>Pinjaman Saat Ini</Text>
+        <Text className='text-2xl font-bold text-[#3e3e3e] mt-2 text-center'>Rp 3.000.000</Text>
+        {/* Info nominal */}
+        <View className='flex-row justify-between mt-5'>
+          <Text className='text-[#3e3e3e]'>Rp {paidLoan.toLocaleString('id-ID')}</Text>
+          <Text className='text-red-600'>Rp {remainingLoan.toLocaleString('id-ID')}</Text>
+        </View>
+
+        {/* Progress Bar */}
+        <View className='w-full h-3 bg-gray-200 rounded-full mt-2 overflow-hidden'>
+          <View
+            className='h-full bg-blue-600 rounded-full'
+            style={{ width: `${progress}%` }}
+          />
+        </View>
+
+        {/* Persentase */}
+        <Text className='text-center mt-2 text-sm text-[#3e3e3e] font-medium'>{progress}% Terbayar</Text>
+      </View>
     </View>
   );
 };
