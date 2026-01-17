956b55de6fa7268ead17b71b2431b9c982dda930
mardin <tredingb25@gmail.com>
2026-01-17 19:43:10 +0700

    refactor: simplify FeatureMenu component styling and structure

--- components/FeatureMenu.tsx
+++ components/FeatureMenu.tsx
@@ -8,10 +8,7 @@ interface FeatureMenuProps {
 
 const FeatureMenu: FC<FeatureMenuProps> = ({ title, children }) => {
   return (
-    <View
-      className='bg-white py-6 rounded-2xl items-center justify-center'
-      style={{ width: 75 }}
-    >
+    <View className='bg-white py-6 rounded-2xl items-center justify-center w-[80px] elevation-sm'>
       {children}
       <Text className={`mt-2 text-sm font-bold text-green-600`}>{title}</Text>
     </View>
